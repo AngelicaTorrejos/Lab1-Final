36f7376e50d78433eb288af5fae28b1e598bef07
Angelica Torrejos <torrejosma.angelica@gmail.com>
2023-05-11 10:44:19 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,93 @@
+<?php
+
+//  <-- CONTROLLER - THE MIDDLE MAN
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request; // <-- handling http request in lumen
+use App\Models\User; // <-- The model
+use App\Traits\ApiResponser; // <-- use to standardized our code for api response
+
+// use DB;  // <---if you're not using lumen eloquent you can use DB component in lumen
+
+class UserController extends Controller
+{
+    use ApiResponser;
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+// GET
+
+    public function all()
+    {
+        // eloquent style
+        $users = User::all();
+
+        // sql string as parameter (if nag use og DB)
+        // $user = DB::connection('mysql')
+        // ->select("Select * from tbluser");
+        return response()->json($users, 200); // <-- before
+        // return $this->successResponse($users);
+    }
+
+// GET (ID)
+public function show($id)
+{ 
+    return User::where('CustomerId', 'like', '%'.$id.'%')->get();
+
+}
+
+// ADD
+public function add(Request $request)
+{
+    
+    $rules = [
+        'CustomerId' => 'required|max:20',
+        'Firstname' => 'required|max:20',
+        'Middlename' => 'required|max:20',
+        'Lastname' => 'required|max:20',
+    ];
+    $this->validate($request, $rules);
+    $user = User::create($request->all());
+
+    return $this->successResponse($user, Response::HTTP_CREATED);
+}
+
+// UPDATE
+public function up(Request $request, $id)
+{
+    $rules = [
+       'CustomerId' => 'required|max:20',
+        'Firstname' => 'required|max:20',
+        'Middlename' => 'required|max:20',
+        'Lastname' => 'required|max:20',
+    ];
+    $this->validate($request, $rules);
+    $user = User::findOrFail($id);
+    $user->fill($request->all());
+
+    $user->save();
+
+    return $user;
+}
+
+// DELETE
+
+public function del($id)
+{
+    $user = User::findOrFail($id);
+    $user->delete();
+}
+
+    // Index
+
+public function index()
+    {
+        $users = User::all();
+
+        return $this->successResponse($users);
+    }
+}
